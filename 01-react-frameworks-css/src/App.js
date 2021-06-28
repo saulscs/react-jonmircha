a11d127e98ca64456b743d2841723318327d7357
Saul Corona <saulcoronascs@gmail.com>
2021-06-27 19:48:11 -0500

    Bulma

--- 01-react-frameworks-css/src/App.js
+++ 01-react-frameworks-css/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Bootstrap from "./Components/Booststrap";
+import Bulma from "./Components/Bulma";
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con React</h1>
       <Bootstrap />
+      <hr />
+      <Bulma />
     </div>
   );
 }
