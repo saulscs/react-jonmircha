1f3a30e707aced3890e0decab13cd3f42d499d07
Saul Corona <saulcoronascs@gmail.com>
2021-06-27 20:17:24 -0500

    React Bootstrap & Material UI

--- 01-react-frameworks-css/src/App.js
+++ 01-react-frameworks-css/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Bootstrap from "./Components/Booststrap";
 import Bulma from "./Components/Bulma";
+import ReactBooststrap from "./Components/ReactBooststrap";
+import MaterialUIReact from "./Components/MaterialUI";
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con React</h1>
+      <p>Descomentar los componentes y cdn para poder ver los estilos</p>
       <Bootstrap />
       <hr />
       <Bulma />
+      <hr />
+      <ReactBooststrap />
+      <hr />
+      <MaterialUIReact />
     </div>
   );
 }
---
+++ 01-react-frameworks-css/src/Components/MaterialUI.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MediaCard from './MaterialCard';
+
+export default function MaterialUIReact() {
+  return (
+    <>
+      <h2>Material UI</h2>
+      <MediaCard />
+    </>
+  );
+}
