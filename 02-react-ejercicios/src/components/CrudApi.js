545aa89fce4ad5e62b2f77d213b724e997099094
Saul Corona <saulcoronascs@gmail.com>
2021-07-02 19:22:23 -0500

    Peticiones HTTP a la API ( 5 / 5 )

--- 02-react-ejercicios/src/components/CrudApi.js
+++ 02-react-ejercicios/src/components/CrudApi.js
@@ -29,23 +29,57 @@ const CrudApi = () => {
   }, [url]);
 
   const createData = (data) => {
-    setDb([...db, data]);
+    let options = {
+      headers: { "content-type": "application/json" },
+      body:data,
+    };
     data.id = Date.now();
+    api.post(url,options).then(res => {
+      if(!res.err){
+        setDb([...db, data]);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const updateData = (data) => {
-    let newData = db.map((el) => (el.id === data.id ? data : el));
-    setDb(newData);
+    let options = {
+      headers: { "content-type": "application/json" },
+      body:data,
+    };
+    let endpoint = `${url}/${data.id}`
+    console.log(endpoint)
+    api.put(endpoint,options).then(res => {
+      if(!res.err){
+        let newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const deleteData = (id) => {
     let isDelete = window.confirm(
-      `¿Estás seguro de eliminar el registro con el id ${id}?`
+      `¿Estás seguro de eliminar el registro con el id '${id}'?`
     );
 
     if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
+      let endpoint = `${url}/${id}`;
+      let options = {
+        headers: { "content-type": "application/json" },
+      };
+
+      api.del(endpoint, options).then((res) => {
+        //console.log(res);
+        if (!res.err) {
+          let newData = db.filter((el) => el.id !== id);
+          setDb(newData);
+        } else {
+          setError(res);
+        }
+      });
     } else {
       return;
     }
