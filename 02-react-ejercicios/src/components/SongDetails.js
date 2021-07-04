2202a2871daf14db33fe363ec17fdf438a5144cf
Saul Corona <saulcoronascs@gmail.com>
2021-07-03 20:32:50 -0500

    Renderizado condicional de datos ( 4 / 5 )

--- 02-react-ejercicios/src/components/SongDetails.js
+++ 02-react-ejercicios/src/components/SongDetails.js
@@ -1,14 +1,29 @@
-import React from "react";
+import Message from "./Message";
 import SongArtist from "./SongArtist";
 import SongLyric from "./SongLyric";
 
 const SongDetails = ({ search, lyric, bio }) => {
+  if (!lyric || !bio) return null;
+
   return (
-    <div>
-      <h2>Detalles</h2>
-      <SongArtist />
-      <SongLyric />
-    </div>
+    <>
+      {lyric.error || lyric.err || lyric.name === "AbortError" ? (
+        <Message
+          msg={`Error: no existe la canción "<em>${search.song}</em>"`}
+          bgColor="#dc3545"
+        />
+      ) : (
+        <SongLyric title={search.song} lyrics={lyric.lyrics} />
+      )}
+      {bio.artists ? (
+        <SongArtist artist={bio.artists[0]} />
+      ) : (
+        <Message
+          msg={`Error: no existe el intérprete '<em>${search.artist}</em>'`}
+          bgColor="#dc3545"
+        />
+      )}
+    </>
   );
 };
 
--- 02-react-ejercicios/src/components/SongSearch.js
+++ 02-react-ejercicios/src/components/SongSearch.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { helpHttp } from "./helpers/helpHttp";
 import SongForm from "./SongForm";
 import SongDetails from "./SongDetails";
 import Loader from "./Loader";
@@ -8,6 +9,29 @@ const SongSearch = () => {
   const [lyric, setLyric] = useState(null);
   const [bio, setBio] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (search === null) return;
+
+    const fetchData = async () => {
+      const { artist, song } = search;
+
+      let artistUrl = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+      let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+      console.log(artistUrl, songUrl);
+
+      setLoading(true);
+      const [artistRes, songRes] = await Promise.all([
+        helpHttp().get(artistUrl),
+        helpHttp().get(songUrl),
+      ]);
+      console.log(artistRes, songRes);
+      setBio(artistRes);
+      setLyric(songRes);
+      setLoading(false);
+    };
+    fetchData();
+  }, [search]);
 
   const handleSearch = (data) => {
     console.log(data);
@@ -19,7 +43,9 @@ const SongSearch = () => {
       <h2>Song Search</h2>
       {loading && <Loader />}
       <SongForm handleSearch={handleSearch} />
-      <SongDetails search={search} lyric={lyric} bio={bio} />
+      {search && !loading && (
+        <SongDetails search={search} lyric={lyric} bio={bio} />
+      )}
     </div>
   );
 };
