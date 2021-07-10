c875ebbdff688b24466f8f9d477b6d0aeae36ef1
Saul Corona <saulcoronascs@gmail.com>
2021-07-10 15:47:44 -0500

    Curso React: 48. Validación Formularios: Envío de datos AJAX y API FormSubmit ( 3 / 4 )

--- 02-react-ejercicios/src/components/ContacForm.js
+++ 02-react-ejercicios/src/components/ContacForm.js
@@ -10,9 +10,30 @@ const initialForm = {
 
 const validationForm = (form) => {
   let errors = {};
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let regexComments = /^.{1,255}$/;
 
   if (!form.name.trim()) {
     errors.name = "El campo 'Nombre' es requerido";
+  } else if (!regexName.test(form.name.trim())) {
+    errors.name = "El campo 'Nombre' solo acepta letras y espacios en blanco"
+  }
+
+  if (!form.email.trim()) {
+    errors.email = "El campo 'Email' es requerido";
+  } else if (!regexEmail.test(form.email.trim())) {
+    errors.email = "El campo 'Email' es incorrecto"
+  }
+
+  if (!form.subject.trim()) {
+    errors.subject = "El campo 'Asunto a tratar' es requerido";
+  }
+
+  if (!form.comments.trim()) {
+    errors.comments = "El campo 'Comentarios' es requerido";
+  } else if (!regexComments.test(form.comments.trim())) {
+    errors.comments = "El campo 'Comentarios' no debe exceder los 255 caracteres "
   }
 
   return errors;
