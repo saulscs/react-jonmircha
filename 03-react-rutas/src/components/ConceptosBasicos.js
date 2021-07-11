a9e138d8b32df32828760505e6e2ef7d746ade36
Saul Corona <saulcoronascs@gmail.com>
2021-07-11 17:57:37 -0500

    Componentes Link y NavLink

--- 03-react-rutas/src/components/ConceptosBasicos.js
+++ 03-react-rutas/src/components/ConceptosBasicos.js
@@ -3,22 +3,44 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Acerca from "../pages/Acerca";
 import Contacto from "../pages/Contacto";
+import Home from "../pages/Home";
+import Error404 from "../pages/Error404";
+import MenuConceptos from "../components/MenuConceptos";
 
 const ConceptosBasicos = () => {
   return (
     <div>
       <h2>Conceptos Basicos</h2>
       <Router>
+        <MenuConceptos />
         <Switch>
-          <Route exact path="/">
-            <h3>Home</h3>
-          </Route>
+          <Route exact path="/" component={Home} />
           <Route exact path="/acerca" component={Acerca} />
           <Route exact path="/contacto" component={Contacto} />
+          <Route path="*" component={Error404} />
         </Switch>
       </Router>
     </div>
   );
 };
+
+//Conceptos basicos de enrutamiento
+
+// const ConceptosBasicos = () => {
+//   return (
+//     <div>
+//       <h2>Conceptos Basicos</h2>
+//       <Router>
+//         <Switch>
+//           <Route exact path="/">
+//             <h3>Home</h3>
+//           </Route>
+//           <Route exact path="/acerca" component={Acerca} />
+//           <Route exact path="/contacto" component={Contacto} />
+//         </Switch>
+//       </Router>
+//     </div>
+//   );
+// };
 
 export default ConceptosBasicos;
---
+++ 03-react-rutas/src/pages/Error404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <div>
+      <p>Not found</p>
+    </div>
+  );
+};
+
+export default Error404;
