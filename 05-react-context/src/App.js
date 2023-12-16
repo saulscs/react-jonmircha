0d4346898a5ee51f20c22da78d460a0c954573a3
Saul Corona <saulcoronascs@gmail.com>
2023-12-16 14:14:31 -0600

    Context API 1

---
+++ 05-react-context/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyPage from './components/MyPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MyPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 05-react-context/src/components/MyPage.js
@@ -0,0 +1,18 @@
+const MyPage = () => {
+    return (
+        <div className="my-page">
+            <header>
+                <h2>Mi aplicación SIN context API</h2>
+                <h3>Mi cabecera</h3>
+            </header>
+            <main>
+                <p>Mi contenido principal</p>
+            </main>
+            <footer>
+                <h4>Pie de pagina</h4>
+            </footer>
+        </div>
+    )
+}
+
+export default MyPage;
